48e8fa254b3849956ed395f5fa3de9154b422fdb
James Mattos <jamesmattos55@yahoo.com>
2019-01-22 14:57:15 -0700

    Initial commit

---
+++ src/edu/cnm/deepdive/Gauss.java
@@ -0,0 +1,23 @@
+package edu.cnm.deepdive;
+
+public class Gauss {
+
+  public static final int DEFAULT_UPPER_LIMIT = 100;
+
+  public static void main(String[] args) {
+    
+    int upperLimit = DEFAULT_UPPER_LIMIT;
+    long sum = triangleSum(upperLimit);
+    System.out.printf("Sum from 1 to %,d  = %,d%n", upperLimit, sum);
+
+  }
+
+  public static long triangleSum(int upperLimit) {
+    long sum = 0;
+    for (int i = 1; i <= upperLimit; i++) {
+      sum += i;
+    }
+    return sum;
+  }
+
+}
